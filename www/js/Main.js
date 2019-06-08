544894f0f009dbd1311efbbc4de81bc1b346b428
dk <decay@mail.de>
2019-06-08 12:57:04 +0200

    added new viewcontroller

--- www/js/Main.js
+++ www/js/Main.js
@@ -37,6 +37,7 @@ import * as entities from "./model/MyEntities.js";
 import MyInitialViewController from "./controller/MyInitialViewController.js";
 // TODO-REPEATED: import any further view controllers here
 import ListviewViewController from "./controller/ListviewViewController.js";
+import ReadviewViewController from "./controller/ReadviewViewController.js";
 
 
 // we export the framework modules required by the application and the application modules required by the framework
@@ -55,7 +56,8 @@ export {
     entities,
     MyInitialViewController,
     // TODO-REPEATED: export any further view controllers here
-    ListviewViewController
+    ListviewViewController,
+    ReadviewViewController
 }
 
 // then start the application
---
+++ www/js/controller/ReadviewViewController.js
@@ -0,0 +1,69 @@
+/**
+ * @author Jörn Kreutel
+ */
+import {mwf} from "../Main.js";
+import {entities} from "../Main.js";
+
+export default class ReadviewViewController extends mwf.ViewController {
+
+    constructor() {
+        super();
+
+        console.log("ReadviewViewController()");
+    }
+
+    /*
+     * for any view: initialise the view
+     */
+    async oncreate() {
+        // TODO: do databinding, set listeners, initialise the view
+
+
+        // call the superclass once creation is done
+        super.oncreate();
+    }
+
+    /*
+     * for views with listviews: bind a list item to an item view
+     * TODO: delete if no listview is used or if databinding uses ractive templates
+     */
+    bindListItemView(viewid, itemview, item) {
+        // TODO: implement how attributes of item shall be displayed in itemview
+    }
+
+    /*
+     * for views with listviews: react to the selection of a listitem
+     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
+     */
+    onListItemSelected(listitem, listview) {
+        // TODO: implement how selection of listitem shall be handled
+    }
+
+    /*
+     * for views with listviews: react to the selection of a listitem menu option
+     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
+     */
+    onListItemMenuItemSelected(option, listitem, listview) {
+        // TODO: implement how selection of option for listitem shall be handled
+    }
+
+    /*
+     * for views with dialogs
+     * TODO: delete if no dialogs are used or if generic controller for dialogs is employed
+     */
+    bindDialog(dialogid, dialog, item) {
+        // call the supertype function
+        super.bindDialog(dialogid, dialog, item);
+
+        // TODO: implement action bindings for dialog, accessing dialog.root
+    }
+
+    /*
+     * for views that initiate transitions to other views
+     */
+    async onReturnFromSubview(subviewid, returnValue, returnStatus) {
+        // TODO: check from which view, and possibly with which status, we are returning, and handle returnValue accordingly
+    }
+
+}
+
